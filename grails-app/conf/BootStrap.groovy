ae0a8a02d66796b9240d9d9cb957f53c0fa906e0
Leandro de Assis <leandrodiassis@gmail.com>
2011-07-26 17:16:58 -0300

    production enviroment

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import sismetas.Usuario
 class BootStrap {
 
     def init = { servletContext ->
-		if(!Usuario.findByNome("admin") {
+		if(!Usuario.findByNome("admin")) {
 			println("Cadastrando admin")
 			new Usuario(nome:"admin", senha:new String("admin123".encodeAsMD5Hex()), email:"admin@admin.com", admin:true).save(failOnError: true)
 		}
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,31 +1,19 @@
-/*dataSource {
-    pooled = true
-	dbCreate = "update"
-	url = "jdbc:mysql://192.168.1.250/sismetas"
-    driverClassName = "com.mysql.jdbc.Driver"
-    username = "sismetas"
-    password = "sismetas@password"
-	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-}*/
-
-dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
-}
-hibernate {
+/*hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-}
+}*/
 // environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             url = "jdbc:hsqldb:mem:devDB"
-        }
+			pooled = true
+			driverClassName = "org.hsqldb.jdbcDriver"
+			username = "sa"
+			password = ""
+		}
     }
     test {
         dataSource {
@@ -35,8 +23,13 @@ environments {
     }
     production {
         dataSource {
+			pooled = true
             dbCreate = "update"
+			driverClassName = "com.mysql.jdbc.Driver"
             url = "jdbc:mysql://mysql.cge.ifpb.edu.br/sismetas"
+			username = "sismetas"
+			password = "sismetas@password"
+			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
         }
     }
 }
